deea3e5bad5207470f089f496e442ac9dc75086f
matiasadell <matiasadell@hotmail.com>
2025-11-20 23:52:36 -0300

    siii

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -22,7 +22,7 @@ class CustomException(Exception):
     # No usa self ni datos de clase; solo parámetros. Por eso no depende de estado.
     @staticmethod
     def get_detailed_error_message(error_message, error_detail:sys):
-        _ , _ , exc_tb = error_detail.exc_info()
+        _ , _ , exc_tb = traceback.sys.exc_info()
         file_name = exc_tb.tb_frame.f_code.co_filename
         line_number = exc_tb.tb_lineno
 
--- src/model_training.py
+++ src/model_training.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import RandomizedSearchCV
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from src.logger import get_logger
-from custom_exception import CustomException
+from src.custom_exception import CustomException
 from config.paths_config import *
 from config.model_params import *
 from utils.common_functions import read_yaml, load_data
